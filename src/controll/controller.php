6d70d327262fa10ab2837e32760348e2c60f4e51
Dominik Burgstaller <Dominik.Burgstaller@hakneumarkt.at>
2023-03-13 08:52:06 +0100

    Datenbankverbindung und Controller erstellt

---
+++ src/controll/controller.php
@@ -0,0 +1,24 @@
+<?php
+    class Controller{
+
+        function __construct(){
+
+        }
+
+        function DBConnect_Erstellen(){
+            if(!isset($_SESSION['dbLeser'])){
+                $_SESSION['dbLeser'] = new DBConnect();
+                $_SESSION['dbLeser']->VerbindungAufbauen("publicchatdb", "benutzer");
+            }
+        }
+
+        function Ueberschrift($rang, $ueberschrift){
+                    echo "<h".$rang." class='ueberschrift'>".$ueberschrift."</h".$rang.">";
+        }
+
+        function InputFeldErstellen($feldBeschreibung, $type, $name){
+            echo '<label for="'.$name.'">'.$feldBeschreibung.'</label>';
+            echo '<input type="'.$type.'" id="'.$name.'" name="'.$name.'">';
+        }
+
+?>
---
+++ src/model/dbConnect.php
@@ -0,0 +1,20 @@
+﻿<?php
+    class DBConnect{
+
+        function __construct(){
+
+        }
+
+        function VerbindungAufbauen($datenbank, $tabellenname){
+            
+            $pdo = new PDO('mysql:host=localhost;dbname='.$datenbank.'', 'root', '');
+
+            $sql = "select * from $tabellenname";
+            $statement = $pdo->prepare($sql);
+            $statement->execute();
+
+            $pdo = null;
+
+        }
+    }
+?>
