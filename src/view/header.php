6dbb787cc8c52855c5d38e36ef6a49cee8f5f7c0
Tim Fuerth <Tim.Fuerth@hakneumarkt.at>
2023-03-14 08:59:41 +0100

    header design update

--- src/view/header.php
+++ src/view/header.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="de-at">
 <head>
-	<title>WebCation Schools</title>
+	<title>PublicChat</title>
 	<link rel="stylesheet" href="styles/styles.css">
 	<link rel="icon" type="image/x-icon" href="images/favicon.ico">
 	<meta lang="de-at"/>
 	<meta http-equiv="Content-Type" 
       content="text/html; charset=utf-8">
-	<meta name="description" content="Einfaches Finden und Bewerben f�r Praktikumspl�tze"/>
+	<meta name="description" content="Einfaches Finden und Bewerben für Praktikumsplätze"/>
 	
 	<!--<meta name="google-site-verification" content="">-->
 
@@ -15,8 +15,14 @@
 
 <body>
 	<header>
-			<a href="index.php"><img src="images/book.png" alt="Icon" /></a>
-			<h1>PublicChat</h1>
+		<div id="headerl">
+		<a href="index.php"><img src="" class="logo" alt=""></a>
+		<h1>PublicChat</h1>
+		</div>
+		<div id="headerr">
+
+		</div>
+			
 	</header>
 </body>
 </html>
