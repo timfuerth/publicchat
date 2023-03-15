b6dd85c68366b95dba184f343fd7f986b9e7a3a5
Tim Fuerth <Tim.Fuerth@hakneumarkt.at>
2023-03-15 11:25:11 +0100

    code cleanup

--- src/view/chatTemplate.php
+++ src/view/chatTemplate.php
@@ -8,16 +8,16 @@
                 
             </div>
             <div id="spalter">
-                <section>
-
-
-
-                </section>
                 <form method="post" class="NachrichtSenden">
                     <input type="text" name="msgbox" id="msgbox" placeholder="Schreib eine Nachricht" />
-
+                    <input type="submit" value="Senden">
                 </form>
+                <?php 
+                if (isset($_POST["msgbox"])){
+                    $_SESSION['controller']->msgSenden($_POST["msgbox"]);
+                }
 
+                ?>
             </div>
         </section>
     </div>
